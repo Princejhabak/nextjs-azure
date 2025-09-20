d25d4fbbe8171ee8d73bdfc504c6b1c1b49e2440
Prince Jhabak <princejhabak11.pj@gmail.com>
2025-09-20 17:51:07 +0530

    correct env vars

--- app/components/SampleComponent.tsx
+++ app/components/SampleComponent.tsx
@@ -3,13 +3,13 @@
 const SampleComponent = () => {
 
   console.log("CLIENT_SIDE", process.env["CLIENT_SIDE"]);
-  console.log("NEXT_PULBIC_CLIENT_SIDE", process.env["NEXT_PULBIC_CLIENT_SIDE"]);
+  console.log("NEXT_PUBLIC_CLIENT_SIDE", process.env["NEXT_PUBLIC_CLIENT_SIDE"]);
 
   return (
     <div>
        <p>Client Rendered page</p>
         <p>CLIENT_SIDE - {process.env["CLIENT_SIDE"]}</p>
-        <p>NEXT_PULBIC_CLIENT_SIDE - {process.env["NEXT_PULBIC_CLIENT_SIDE"]}</p>
+        <p>NEXT_PUBLIC_CLIENT_SIDE - {process.env["NEXT_PUBLIC_CLIENT_SIDE"]}</p>
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
 
   console.log("SERVER_SIDE", process.env["SERVER_SIDE"]);
-  console.log("NEXT_PULBIC_SERVER_SIDE", process.env["NEXT_PULBIC_SERVER_SIDE"]);
+  console.log("NEXT_PUBLIC_SERVER_SIDE", process.env["NEXT_PUBLIC_SERVER_SIDE"]);
 
   return (
     <div className={styles.page}>
@@ -11,7 +11,7 @@ export default function Home() {
         <p>Hello World</p>
         <p>Server Rendered page</p>
         <p>SERVER_SIDE - {process.env["SERVER_SIDE"]}</p>
-        <p>NEXT_PULBIC_SERVER_SIDE - {process.env["NEXT_PULBIC_SERVER_SIDE"]}</p>
+        <p>NEXT_PUBLIC_SERVER_SIDE - {process.env["NEXT_PUBLIC_SERVER_SIDE"]}</p>
       </main>
     </div>
   );
